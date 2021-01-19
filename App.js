b6d6386f04cbba66c9ef5cbe15db572a5eac87f9
saad277 <saadsheikh27@gmail.com>
2021-01-19 10:58:42 +0500

    spin interpolate

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Animated, Easing} from 'react-native';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.spinValue = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    this.spin();
+  }
+
+  spin = () => {
+    this.spinValue.setValue(0);
+
+    Animated.timing(this.spinValue, {
+      toValue: 1,
+      duration: 5000,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start(() => this.spin());
+  };
+
+  render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg'],
+    });
+
+
+    // console.log(this.spinValue);
+
+    return (
+      <View style={styles.container}>
+        <Animated.Image
+          style={{
+            width: 227,
+            height: 200,
+            transform: [{rotate: spin}],
+          }}
+          source={{
+            uri:
+              'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png',
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
