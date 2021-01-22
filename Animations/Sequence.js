8f4aac975b880594db2c40c81d15a8c3b880521a
saad277 <saadsheikh27@gmail.com>
2021-01-22 17:57:16 +0500

    sequence

---
+++ Animations/Sequence.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  Animated,
+  StyleSheet,
+  TouchableHighlight,
+  Easing,
+  Button,
+  ScrollView,
+} from 'react-native';
+
+const arr = [];
+
+for (var i = 0; i < 500; i++) {
+  arr.push(i);
+}
+
+class Sequence extends Component {
+  constructor() {
+    super();
+    this.animatedValue = [];
+
+    arr.forEach((value) => {
+      this.animatedValue[value] = new Animated.Value(0);
+    });
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    const animations = arr.map((item) => {
+      return Animated.timing(this.animatedValue[item], {
+        toValue: 1,
+        duration: 50,
+        useNativeDriver: true,
+      });
+    });
+    Animated.sequence(animations).start();
+  }
+
+  render() {
+    const animations = arr.map((x, i) => {
+      return (
+        <Animated.View
+          key={i}
+          style={{
+            opacity: this.animatedValue[x],
+            height: 20,
+            width: 20,
+            backgroundColor: 'red',
+            marginLeft: 3,
+            marginTop: 3,
+          }}
+        />
+      );
+    });
+    return <View style={styles.container}>{animations}</View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+});
+
+export default Sequence;
--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import Spin from './Animations/Spin';
 import Timings from './Animations/TimingExamples';
 import Spring from './Animations/Spring';
 import Parallel from './Animations/ParallelAnimations';
+import Sequence from './Animations/Sequence';
 
 const App = () => {
-  return <Parallel />;
+  return <Sequence />;
 };
 
 export default App;
